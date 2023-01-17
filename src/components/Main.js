3edbbae290dd80cff91972c47efb5bdfea5a7704
Jorge Sanchez <jorgesinsilla@gmail.com>
2023-01-17 15:21:01 +0000

    examples of conditionals

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,19 +12,38 @@ function Main() {
         });
         setMoviesArr(newListOfMovies);
     }
+
+      // Conditional Rendering with "Element Variables"
+  let titleMessage;
+  if(moviesArr.length > 0){
+    titleMessage = <h2>Current number of movies: {moviesArr.length}</h2>;
+  } else {
+    titleMessage = <h2>Sorry, no movies to display</h2>
+  }
+
+
   return (
     <div className="Main">
 
-    <h2>Current number of Movies: {moviesArr.length}</h2>
+    {titleMessage}
 
       {moviesArr.map((movieDetails) => {
         return(
             <div key={movieDetails} className="card movie">
                 <h3>{movieDetails.title}</h3>
+
+                {movieDetails.imgURL 
+                ? <img src={movieDetails.imgURL} alt=""/>
+                : <img src="https://via.placeholder.com/182x268" alt=""/>
+                }
+
                 <p>Rating: {movieDetails.rating}</p>
+
+                {/*  Conditional Rendering with "Logical && Operator"  */}
+                {movieDetails.rating > 8 && <p>Recommended</p>}
                 <p>Year: {movieDetails.year}</p>
                 <p>Genre: {movieDetails.genres[0]}</p>
-                <img src={movieDetails.imgURL} alt=""/>
+
                 <button onClick={() => {deleteMovie(movieDetails.id)}}>Delete</button>
             </div>
             );
